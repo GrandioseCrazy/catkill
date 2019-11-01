b0e491c2e0d91435b92b2e54b5ca0524c7166e28
DoubleP <pengp@corp.21cn.com>
2019-11-01 11:34:10 +0800

    第一次创建你哟

---
+++ src/main/java/com/avaj/ekill/EkillApplication.java
@@ -0,0 +1,13 @@
+package com.avaj.ekill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkillApplication.class, args);
+    }
+
+}
